9fbf4b0f39bcae20eab64665c75500b995a12fb2
Joshua Ike <79171733+kxng0109@users.noreply.github.com>
2025-10-14 10:35:59 +0100

    chore: Initial commit

---
+++ src/main/java/io/github/kxng0109/taskflow/TaskFlowApplication.java
@@ -0,0 +1,13 @@
+package io.github.kxng0109.taskflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskFlowApplication.class, args);
+    }
+
+}
