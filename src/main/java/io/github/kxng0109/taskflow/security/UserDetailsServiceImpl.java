40f57e5cfce9398063197a43ec4048e6073abd01
Joshua Ike <79171733+kxng0109@users.noreply.github.com>
2025-10-17 13:36:10 +0100

    feat(user): Implement UserDetails interface for User entity

--- src/main/java/io/github/kxng0109/taskflow/security/UserDetailsServiceImpl.java
+++ src/main/java/io/github/kxng0109/taskflow/security/UserDetailsServiceImpl.java
@@ -20,13 +20,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User not fou d with email: " + email));
-
-        return new org.springframework.security.core.userdetails.User(
-                user.getEmail(),
-                user.getPassword(),
-                new ArrayList<>()
-        );
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
     }
 }
--- src/main/java/io/github/kxng0109/taskflow/user/User.java
+++ src/main/java/io/github/kxng0109/taskflow/user/User.java
@@ -7,7 +7,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -16,7 +21,7 @@ import java.util.Set;
 @AllArgsConstructor
 @Builder
 @Data
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -35,4 +40,35 @@ public class User {
 
     @OneToMany(mappedBy = "assignee")
     private Set<Task> assignedTasks;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
